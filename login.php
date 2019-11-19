7417f9fc1bff26b57e80b4d383c54f871a943064
Pop2021 <56492147+Pop2021@users.noreply.github.com>
2019-11-19 21:20:37 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action = "addcontactproc.php">
+        Email:<br>
+        <input type="email" name="studEmail"><br>
+        <br>
+        Password:<br>
+        <input type="password" name="Password"><br>
+        <br>
+        <br>
+        <input type="Submit" value="Login" name="Login">
+    </form>
+</body>
+</html>
