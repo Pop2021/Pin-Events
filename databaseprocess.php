829c2a693807e5516304974cbe5844a1cb622c8e
AfuaKB <afua.mkb@gmail.com>
2019-11-20 04:03:57 +0000

    db commit

--- databaseprocess.php
+++ databaseprocess.php
@@ -8,7 +8,7 @@
 
 	//connection 
 	$connection = mysqli_connect($servername,$username,$password,$database);
-
+	
 	// Check connection
 	if (!$connection) {
 		die("Connection failed: " . mysqli_connect_error());
