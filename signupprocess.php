143600ee4f06cf1edcfd855fa4af4d816705fa20
lenryn <lenry.neequaye@ashesi.edu.gh>
2019-11-20 17:26:34 +0000

    Edited signupprocess.php

--- signupprocess.php
+++ signupprocess.php
@@ -1,8 +1,7 @@
 <?php
 
- //Getting form data
- if (isset($_POST['signup_page'])){
-
+//Getting form data
+if (isset($_POST['signup_page'])){
     //echo "works";
     $firstName = $_POST['fname'];
     $lastName = $_POST['lname'];
@@ -10,6 +9,7 @@
     $email = $_POST['email'];
         //$yeargroup = $_POST['yGroup'];
     $password = $_POST['pword'];
+}
 
 
 //declare variables for my database connection 
@@ -43,28 +43,29 @@ if (isset($_POST['signup_page'])) {
     $email = $_POST['email'];
     $username = $_POST['password'];
 
-// encrypt password before storing to database
+    //encrypt password before storing to database
 
-// create sql query to insert user into the database
-$sql = "INSERT INTO user (Username,Password) VALUES('$name','$password')";
+    // create sql query to insert user into the database
+    $sql = "INSERT INTO user (Username,Password) VALUES('$name','$password')";
 
-//run the query and store result
-$result = mysqli_query($connection, $sql);
+    //run the query and store result
+    $result = mysqli_query($connection, $sql);
 
-//check if results were retrieved
-if (mysqli_num_rows($result)) {
-    // redirect user to login page using a session
-    header("Location: login.php");
-} else {
-    // redirect user to register page  using a session
-    header("Location: index.php");
-    // display error
-    echo"Registration Failed. Please try again.";
+    //check if results were retrieved
+    if (mysqli_num_rows($result)) {
+        // redirect user to login page using a session
+        header("Location: login.php");
+    } 
+    else {
+        // redirect user to register page  using a session
+        header("Location: index.php");
+        // display error
+        echo"Registration Failed. Please try again.";
+    }
 }
 
 // close database connection
 $connection->close();
 
-}
 
 ?>
