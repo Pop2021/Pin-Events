de0b380254cf441d445d5f5a47dce0374db17de4
Jacen08 <Jason.Nkansah@ashesi.edu.gh>
2019-11-20 03:10:17 +0000

    test commit

---
+++ db_cred.php
@@ -0,0 +1,8 @@
+<?php
+//Database credentials
+define("DATABASE", "P2021");
+define("SERVER", "localhost");
+define("USERNAME", "root");
+define("PASSWD", "");
+
+?>
---
+++ eventsClass.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 
+ */
+class Event
+{
+    public $eventName = "";
+    public $eventVenue = "";
+    public $eventDate = "";
+    public $eventStartTime = "";
+    public $
+
+	function __construct(argument)
+	{
+		# code...
+	}
+}
+?>
